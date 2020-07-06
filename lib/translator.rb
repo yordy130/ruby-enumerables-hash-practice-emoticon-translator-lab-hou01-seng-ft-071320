4baa1b0fa350f38c62798c2c6d2bfe4b2c6fb296
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-06 23:47:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_english_meaning(file, emoticon)
    library.each do |key, value|
      #binding.pry
      if value[:japanese]
-      key == value
+      
      end
      #binding.pry
    end
