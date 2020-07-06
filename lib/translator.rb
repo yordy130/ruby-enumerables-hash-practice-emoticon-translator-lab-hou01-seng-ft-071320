d403af2eebf895014b1d458c9100af615edf4225
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-06 21:31:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  emoticons.each do |key|
+    #binding.pry
+    if key.each_with_object({}) { |key, value| inner_hash}
+    end
+    
+  end
+  #binding.pry
 end
 
 def get_japanese_emoticon
