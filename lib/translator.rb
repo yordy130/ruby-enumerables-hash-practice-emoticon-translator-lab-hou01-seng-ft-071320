162c5d06b67a3bd403ab229a3f9a2031fa61b607
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-06 21:45:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,6 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   emoticons.each do |key|
     #binding.pry
-    if key.each_with_object({}) { |key, value| inner_hash}
-    end
   end
   #binding.pry
 end
