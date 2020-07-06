a5bf07f8ed2bb92be8a14df2b97584e323facbde
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-06 22:24:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(file_path)
   emoticons.each do |key, value|
     #binding.pry
     emoticons[key] = {}
-    emoticons[key][value]
-    binding.pry
+    emoticons[key][value] = 
+    #binding.pry
   end
   #binding.pry
 end
